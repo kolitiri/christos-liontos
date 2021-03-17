a6e6f399c65c2f42748b938c603ce207b4abc398
chris220688 <13407893+kolitiri@users.noreply.github.com>
2021-03-17 14:25:44 +0000

    Updates

---
+++ precache-manifest.98c26df917d6d36afd5bfd1b4eb4564c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "7666111f91c90753b2c8",
+    "url": "/christos-liontos/static/css/main.b07992ff.chunk.css"
+  },
+  {
+    "revision": "7666111f91c90753b2c8",
+    "url": "/christos-liontos/static/js/main.db87b2ac.chunk.js"
+  },
+  {
+    "revision": "8fe835fde4c707388829",
+    "url": "/christos-liontos/static/js/runtime~main.7e92e8e6.js"
+  },
+  {
+    "revision": "6d2e00e4e20b9cfda3fd",
+    "url": "/christos-liontos/static/js/2.07d4d849.chunk.js"
+  },
+  {
+    "revision": "ef468e275a2c13d0bc7156549261fde6",
+    "url": "/christos-liontos/static/media/Octocat.ef468e27.png"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/christos-liontos/static/media/github.76d344f0.svg"
+  },
+  {
+    "revision": "17217b0d284352ce1410606686e46817",
+    "url": "/christos-liontos/static/media/twitter-logo.17217b0d.webp"
+  },
+  {
+    "revision": "564e4bafcd3b130210a1a9bfb32995a5",
+    "url": "/christos-liontos/static/media/SG-Logo.564e4baf.png"
+  },
+  {
+    "revision": "091b16619cb558b58268614152f62689",
+    "url": "/christos-liontos/static/media/playtech-logo.091b1661.svg"
+  },
+  {
+    "revision": "6e7595aceefae1ded3017f54c759db51",
+    "url": "/christos-liontos/static/media/thes.6e7595ac.jpeg"
+  },
+  {
+    "revision": "8c3064d9010baecd0fa1619bc4fa0ac7",
+    "url": "/christos-liontos/static/media/garmin.8c3064d9.png"
+  },
+  {
+    "revision": "76965a95ee1a1d76bc7fca19276cdff3",
+    "url": "/christos-liontos/static/media/findsources.76965a95.svg"
+  },
+  {
+    "revision": "72ad8bf135d2154813fb5847873bb88f",
+    "url": "/christos-liontos/static/media/pypi-logo.72ad8bf1.svg"
+  },
+  {
+    "revision": "92bd24bf1170f84b60d1137c8d963238",
+    "url": "/christos-liontos/static/media/background.92bd24bf.jpeg"
+  },
+  {
+    "revision": "087c005aaede1abe8eba15c864fb9e10",
+    "url": "/christos-liontos/index.html"
+  }
+];
